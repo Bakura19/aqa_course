61f431e271f22e171a39b79fd67773fc88a28265
Александр Бакурин <a.bakurin@maxbit.local>
2025-03-02 12:49:52 +0300

    HW JS basic

---
+++ js_basic_hw.js
@@ -0,0 +1,44 @@
+
+// Addition
+const testAdd = "Word" + 1;
+console.log(testAdd)
+
+const testAdd1 = "12" + 4;
+console.log(testAdd1);
+
+const testAdd2 = 3 + true;
+console.log(testAdd2)
+
+// Multiplication
+const testMult = '2' * true
+console.log(testMult)
+
+const testMult1 = '4' * 6
+console.log(testMult1)
+
+const testMult2 = 8 * false
+console.log(testMult2)
+
+// Division
+const testDiv = '4' / true
+console.log(testDiv)
+
+const testDiv1 = '5' / 2
+console.log(testDiv1)
+
+const testDiv2 = 8 / true
+console.log(testDiv2)
+
+// Transformation
+
+console.log(Number('777'))
+console.log(Number(true))
+
+console.log(parseInt('23.45Pi'))
+console.log(parseFloat('15.15'))
+
+console.log(String(777))
+console.log(String(false))
+
+console.log(Boolean(1))
+console.log(Boolean(0))
