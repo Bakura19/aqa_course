6f30b5c4a8df460f2f4fe592464e6055402020c1
Александр Бакурин <a.bakurin@maxbit.local>
2025-03-24 18:05:32 +0300

    Locators HW

---
+++ locators.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ 1. 
+//button[@aria-label="Search BBC"] 
+[aria-label="Search BBC"] 
+
+2. 
+//*[@data-testid="level1-navigation-container"]//nav
+[data-testid*="navigation-container"] nav
+
+3. (//*[@data-testid="dundee-card"]//*[@data-testid="card-metadata-lastupdated"])[2]
+[data-testid="first-grid"] [data-testid="dundee-card"]:nth-child(2) [data-testid="card-metadata-lastupdated"] 
+
+4. 
+//div[@data-testid="westminster-card"]//div[@data-testid="card-media-wrapper"]
+[data-testid="westminster-card"] img
+
+6.
+(//*[@data-testid="manchester-card"]//*[@data-testid="card-metadata-lastupdated"])[4]
+[data-testid="second-grid"] [data-testid="manchester-card"]:nth-child(3) [data-testid="card-metadata-lastupdated"] 
+
+7. //div[@data-testid="second-grid"]/div[4]
+ [data-testid="second-grid"] [data-testid="manchester-card"]:nth-of-type(4)
+ */
